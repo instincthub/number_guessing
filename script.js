ec5147954f894156a85ff09855d84ff17a75f618
noaholatoye <noaholatoye@gmail.com>
2022-05-07 05:38:44 +0100

    Adding variables to store our data

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your JavaScript goes here
+
+// Adding variables to store our data
+let random_number = Math.floor(Math.random() * 100) + 1;
+
+const guessesList = document.querySelector('.guesses-list');
+const lastResult = document.querySelector('.last-result');
+const lowOrHi = document.querySelector('.low-or-hi');
+
+const guessSubmit = document.querySelector('.guess-submit');
+const guessField = document.querySelector('.field-input');
+
+let guess_count = 1;
+let reset_button;
